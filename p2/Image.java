3f35c41dd8a52f6f422089450cfdc92433e96f29
GolamMortuzaSourov <103216830+GolamMortuzaSourov@users.noreply.github.com>
2023-11-15 12:00:22 -0500

    Add files via upload

---
+++ p2/Image.java
@@ -0,0 +1,10 @@
+public class Image<T extends Comparable<T>> implements Iterable<Node<T>>
+{
+    /**
+        1. define a constructor that creates the two-dimensional doubly-linked list
+        2. define the three getters as described in the specs
+        3. define the two iterator methods as described in the specs
+        4. define the six image manipulaton methods as described in the specs
+    */
+
+}
---
+++ p2/Node.java
@@ -0,0 +1,10 @@
+public final class Node<T extends Comparable<T>> implements Comparable<Node<T>>
+{
+    /**
+        1. declare the five private instance variables as described in the specs
+        2. declare the two constructors as described in the specs
+        3. declare the five getter methods as described in the specs
+        4. declare the five setter methods as described in the specs
+        5. declare the compareTo required by the implementation of the Comparable<Node<T>> interface
+    */    
+}
